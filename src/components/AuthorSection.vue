00ae510f75cad262a93c9a57b5108b4f698f9542
kequnyu <x2era.com>
2026-04-23 06:58:30 +0800

    a

--- src/components/AuthorSection.vue
+++ src/components/AuthorSection.vue
@@ -1,30 +1,20 @@
 <template>
   <n-card title="作者" size="small" :bordered="true">
-    <n-image
-      :src="authorImageUrl"
-      alt="作者"
-      object-fit="cover"
-      preview-disabled
-      class="author-img"
-    />
+    <img :src="authorImageUrl" alt="作者" class="author-img" />
   </n-card>
 </template>
 
 <script setup>
-import { resolveAssetUrl } from '../utils/assetUrl.js'
-
-const authorImageUrl = resolveAssetUrl('zsm.jpg')
+/** 构建时由 Vite 解析为带 base 的 assets 路径，子路径部署（如 GitHub Pages）可靠 */
+import authorImageUrl from '../assets/zsm.jpg?url'
 </script>
 
 <style scoped>
 .author-img {
   width: 100%;
-  border-radius: var(--n-border-radius);
-  overflow: hidden;
-}
-
-.author-img :deep(img) {
-  width: 100%;
   display: block;
+  border-radius: var(--n-border-radius);
+  object-fit: cover;
+  aspect-ratio: 1;
 }
 </style>
