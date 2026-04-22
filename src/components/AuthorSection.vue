c43b5bb01b9f88015b8e79817f189ff52b70c2f6
kequnyu <x2era.com>
2026-04-23 06:45:59 +0800

    a

--- src/components/AuthorSection.vue
+++ src/components/AuthorSection.vue
@@ -13,7 +13,7 @@
 <script setup>
 import { resolveAssetUrl } from '../utils/assetUrl.js'
 
-const authorImageUrl = resolveAssetUrl('zsm.jpg')
+const authorImageUrl = resolveAssetUrl('public/zsm.jpg')
 </script>
 
 <style scoped>
